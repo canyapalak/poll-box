551c709de5138d2d059cec82e6bffdf7442cdbbe
canyapalak <canyapalak@gmail.com>
2023-03-19 22:29:31 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import LogoLight from "./assets/logo-light.png";
+import { BsFillMoonFill } from "react-icons/bs";
+import PollsBoard from "./components/PollsBoard";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "./config/FirebaseConfig.js";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [allPolls, setAllPolls] = useState([]);
+
+  useEffect(() => {
+    async function getPolls() {
+      try {
+        const querySnapshot = await getDocs(collection(db, "polls"));
+        const polls = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          title: doc.data().title,
+          totalVotes: doc.data().totalVotes,
+          choices: doc.data().choices,
+          category: doc.data().category,
+          postTime: new Date(
+            doc.data().postTime.seconds * 1000 +
+              doc.data().postTime.nanoseconds / 1000000
+          ).toLocaleString(),
+        }));
+        setAllPolls(polls);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    getPolls();
+  }, []);
+
+  console.log("allPolls", allPolls);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen mx-8 2xl:mx-80 xl:mx-60 lg:mx-40 md:mx-30 sm:mx-20 flex-row justify-center">
+      <div className="flex flex-row justify-around px-5 xl:px-40 lg:px-20 md:px-10 py-5 border-2 border-stone-800">
+        <img
+          src={LogoLight}
+          alt="Logo"
+          className="w-[12rem] h-[6rem] mx-auto"
+        />
+        <BsFillMoonFill className="text-2xl mt-7 mx-auto" />
+      </div>
+      <PollsBoard allPolls={allPolls} />
     </div>
   );
 }
---
+++ src/components/PollsBoard.js
@@ -0,0 +1,53 @@
+import "../index.js";
+import LogoPo from "../assets/icon-politics.png";
+import LogoSp from "../assets/icon-sports.png";
+import LogoHe from "../assets/icon-health.png";
+import LogoAr from "../assets/icon-art.png";
+import LogoBu from "../assets/icon-business.png";
+import LogoLi from "../assets/icon-life.png";
+import LogoTe from "../assets/icon-tech.png";
+import LogoPr from "../assets/icon-products.png";
+import LogoOt from "../assets/icon-other.png";
+
+function PollsBoard({ allPolls }) {
+  const categories = [
+    { category: "politics", icon: LogoPo },
+    { category: "sports", icon: LogoSp },
+    { category: "health", icon: LogoHe },
+    { category: "art", icon: LogoAr },
+    { category: "tech", icon: LogoTe },
+    { category: "products", icon: LogoPr },
+    { category: "life", icon: LogoLi },
+    { category: "other", icon: LogoOt },
+    { category: "business", icon: LogoBu },
+  ];
+
+  function CategoryIcon({ category }) {
+    const selectedCategory = categories.find((c) => c.category === category);
+    const icon = selectedCategory ? selectedCategory.icon : "";
+
+    return <img src={icon} alt={category} className="category-icon" />;
+  }
+
+  return (
+    <div className="bg-sky-100 p-5 flex flex-col gap-3 border-solid border-2 border-ra rounded-lg border-stone-700">
+      {allPolls?.map((poll) => (
+        <div
+          key={poll.id}
+          className="bg-amber-100 flex flex-row gap-5 border-solid border-2 border-ra rounded-lg border-stone-700"
+        >
+          <div className="w-[3rem] h-[3rem] p-2 bg-stone-300 rounded-l-lg border-r-2 border-stone-800 shrink-0">
+            {" "}
+            <CategoryIcon category={poll.category} className="" />
+          </div>
+          <div className="flex flex-row my-auto gap-5 justify-between w-full">
+            <h3 className="">{poll.title}</h3>
+            <p className="mr-3">{poll.totalVotes} votes</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default PollsBoard;
